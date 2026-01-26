5dc7c1028b4280aac7c1bc67bdfa74c352eb10c1
sophiavl <102966872+sophiavl@users.noreply.github.com>
2026-01-26 15:45:33 +0100

    responsive projects

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -16,36 +16,43 @@ function Projects() {
     {
       title: "VIRTUAL REALITY FOR LEARNING DUTCH",
       description:
-        "A virtual reality experience designed for children of refugees to learn Dutch in an engaging and interactive way. Each scene is modeled on real-life environments, allowing learners to encounter vocabulary that reflects their daily lives. In every scene, children pick up objects, hear the word pronounced, and see it spelled out, reinforcing both listening and reading skills. The goal was to create an accessible, fun, and immersive way to learn Dutch.",
-      image: "/images/VR-foto.jpg",
+        "A virtual reality experience that helps children of refugees learn Dutch in an engaging way. Set in familiar, real-life environments, learners interact with objects to hear and see words, reinforcing both listening and reading skills. The goal was to create an accessible, fun, and immersive learning experience.",
+      mediaType: "video",
+      mediaSrc: "/images/demo_spinoza_groep1.mp4",
       skills: ["BLENDER", "UNITY"],
     },
     {
       title: "GRADUATION PROJECT - JUST / LITERUURMUSEUM",
       description:
-        "For my graduation project at the Literatuurmuseum, I redesigned the navigation structure of the museum’s digital exhibitions. I focused on improving usability and consistency across different collections by translating complex content into a clear, intuitive interface. Through user research, interaction design, and iterative prototyping in Figma, I created a navigation system that helps visitors understand where they are, what they can explore, and how exhibitions relate to each other. The result is a more accessible and user-friendly experience for a diverse audience.",
-      image: "/images/literatuurmuseum.png",
+        "For my graduation project at the Literatuurmuseum, I redesigned the navigation structure of the digital exhibitions. Using user research, interaction design, and iterative prototyping in Figma, I created a clearer and more consistent interface that helps visitors understand the structure and relationships between collections, resulting in a more accessible user experience.",
+      mediaType: "image",
+
+      mediaSrc: "/images/literatuurmuseum.png",
       skills: ["FIGMA", "UI/UX DESIGN", "VUE"],
     },
     {
       title: "INTERNSHIP - CLAPPFORM",
       description:
-        "During my internship at Clappform, I worked on a data visualization tool that gives users insight into databases, data relationships, and role-based access control. Using Vue and Nuxt, I helped build interactive visualizations for ERDs, data lineage, and queries, translating complex technical structures into understandable UI components. My work focused on creating scalable, maintainable front-end solutions that make complex data and permissions clear and usable for non-technical users.",
-      image: "/images/Clappform.png",
+        "During my internship at Clappform, I worked on a data visualization tool that provides insight into databases, data relationships, and role-based access control. Using Vue and Nuxt, I helped build interactive visualizations for ERDs, data lineage, and queries, turning complex technical structures into clear, usable interfaces for non-technical users.",
+      mediaType: "image",
+      mediaSrc: "/images/Clappform.png",
       skills: ["FIGMA", "VUE", "VUEFLOW", "NUXT"],
     },
     {
       title: "THIS WEBSITE",
       description:
         "I designed and developed this portfolio from scratch, starting with the visual design and interaction concepts in Figma and translating them into a responsive React application. Using GSAP, I created subtle, scroll-based animations that guide the user through my work and enhance the overall experience without distracting from the content. The focus of this project was to display my projects in an engaging way.",
-      image: "/images/portfolio-website.png",
+      mediaType: "image",
+
+      mediaSrc: "/images/portfolio-website.png",
       skills: ["FIGMA", "REACT", "GSAP"],
     },
     {
       title: "ADVENT CALENDER",
       description:
         "I designed and built an online advent calender to practise frontend development in a fun way.  Inspired by the joy of opening a new advent door each day, this project delivers a daily “piece of internet”, counting down to Christmas. Take a look at it here:",
-      image: "/images/advent.png",
+      mediaType: "image",
+      mediaSrc: "/images/advent.png",
       skills: ["FIGMA", "VUE"],
       link: "https://adventkalender-sophiavls-projects.vercel.app/",
     },
@@ -74,7 +81,7 @@ function Projects() {
       ref={containerRef}
       className='pin-container h-dvh flex flex-col justify-between pt-12 px-4 md:px-8 md:pt-16 2xl:px-14 2xl:gap-6'
     >
-      <div className='flex flex-col justify-between gap-2'>
+      <div className='flex flex-col justify-between pb-3 gap-2'>
         <h1 className='text-(--color-accent)'>PROJECTS</h1>
         <div className='flex h-[5dvh] w-3/4 justify-between md:w-1/2'>
           {projects.map((_, i) => (
@@ -101,40 +108,53 @@ function Projects() {
               className='project-slide w-screen h-full flex items-start gap-12'
             >
               {/* Hier begint de projectcard */}
-              <div className='flex flex-col pb-4 w-[90%] h-[90%]'>
-                <div className='flex flex-col mb-2 gap-2'>
-                  <h3>{project.title}</h3>
+              <div className='flex flex-col border border-(--color-secondary) bg-(--color-primary) w-[90%] h-[98%]'>
+                <div className='flex flex-col justify-center'>
+                  <h3 className='pl-2'>{project.title}</h3>
                   <div className='h-px w-full bg-(--color-secondary)'></div>
                 </div>
                 <div className='flex flex-col justify-between w-full gap-6 h-full'>
-                  <div className='flex flex-col gap-2 lg:gap-6 xl:flex-row '>
-                    <div
-                      className='w-full bg-cover h-[20vh] md:h-[40vh] md:w-full order-1 lg:w-[30vw] lg:h-[30vw] lg:order-2 xl:w-[40vw] xl:h-[60vh]'
-                      style={{ backgroundImage: `url(${project.image})` }}
-                    ></div>
+                  <div className='flex flex-col gap-2 lg:gap-6 lg:flex-row '>
+                    <div className='w-full h-42 md:h-[30%] order-1 overflow-hidden lg:order-2 lg:w-1/2 lg:h-full'>
+                      {project.mediaType === "video" ? (
+                        <video
+                          src={project.mediaSrc}
+                          autoPlay
+                          muted
+                          loop
+                          playsInline
+                          className='w-full h-full object-cover'
+                        />
+                      ) : (
+                        <img
+                          src={project.mediaSrc}
+                          alt={project.title}
+                          className='w-full h-full object-cover'
+                        />
+                      )}
+                    </div>
 
                     <div className='flex flex-col order-2 lg:order-1 lg:w-1/2 lg:gap-2'>
-                      <p className='card-desc'>{project.description}</p>
+                      <p className='card-desc pl-2'>{project.description}</p>
                       {project.link && (
                         <a
                           href={project.link}
                           target='_blank'
                           rel='noopener noreferrer'
-                          className='text-accent underline mt-2 card-desc'
+                          className='text-accent pl-2 underline mt-2 card-desc'
                         >
                           {project.link}
                         </a>
                       )}
+                      <div className='flex flex-wrap pl-2 gap-2 pt-4 lg:w-[40%]'>
+                        {project.skills.map((skill) => (
+                          <Tag
+                            key={skill}
+                            label={skill}
+                          />
+                        ))}
+                      </div>
                     </div>
-                  </div>
-
-                  <div className='flex flex-wrap gap-2 lg:w-[40%]'>
-                    {project.skills.map((skill) => (
-                      <Tag
-                        key={skill}
-                        label={skill}
-                      />
-                    ))}
                   </div>
                 </div>
               </div>
